0b58eb0ff3962fff4aaade9b704ac6502169ef3f
leonsal <leonel.salvadori@gmail.com>
2023-02-23 16:19:08 -0300

    ..

--- gux/src/gb.rs
+++ gux/src/gb.rs
@@ -3,6 +3,7 @@ use std::ffi::CString;
 use std::os::raw::c_char;
 
 #[repr(C)]
+#[derive(Clone,Default)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
@@ -10,6 +11,7 @@ pub struct Vec2 {
 
 // Vector with 4 components
 #[repr(C)]
+#[derive(Clone,Default)]
 pub struct Vec4 {
     pub x: f32,
     pub y: f32,
@@ -70,6 +72,7 @@ type RGBA = u32;
 
 // Vertex info
 #[repr(C)]
+#[derive(Clone,Default)]
 pub struct Vertex {
     pub pos: Vec2,
     pub uv: Vec2,
@@ -89,13 +92,13 @@ pub struct DrawCmd {
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-pub struct DrawList {
-    pub buf_cmd: *mut DrawCmd,
-    pub cmd_count: u32,
-    pub buf_idx: *mut u32,
-    pub idx_count: u32,
-    pub buf_vtx: *mut Vertex,
-    pub vtx_count: u32,
+struct DrawListC {
+    buf_cmd: *const DrawCmd,
+    cmd_count: u32,
+    buf_idx: *const u32,
+    idx_count: u32,
+    buf_vtx: *const Vertex,
+    vtx_count: u32,
 }
 
 // Cursor types
@@ -111,6 +114,56 @@ pub enum Cursor {
     _Count,
 }
 
+// DrawList contains lists of commands and buffers for the graphics backend
+pub struct DrawList {
+ 	buf_cmd: Vec<DrawCmd>, // Buffer with draw commands
+ 	buf_idx: Vec<u32>,
+ 	buf_vtx: Vec<Vertex>,
+ 	path:    Vec<Vec2>,
+ }
+
+impl DrawList {
+
+    pub fn new() -> DrawList {
+        DrawList{
+            buf_cmd: Vec::new(),
+            buf_idx: Vec::new(),
+            buf_vtx: Vec::new(),
+            path: Vec::new(),
+        }
+    }
+
+    //pub fn new_cmd(&self, idx_count: u32, vtx_count: u32) -> (DrawCmd, &[u32], &[Vertex]) {
+    pub fn new_cmd<'a>(&'a mut self, idx_count: u32, vtx_count: u32) -> (&mut DrawCmd, &mut[u32], &mut[Vertex]) {
+
+        // Reserve space for indices
+	    let idx_offset = self.buf_idx.len();
+        self.buf_idx.resize(idx_offset + idx_count as usize, 0);
+
+        // Reserve space for vertices
+        let vtx_offset = self.buf_vtx.len();
+        self.buf_vtx.resize(vtx_offset + vtx_count as usize, Vertex::default());
+
+	    // Creates and appends new command to the DrawList command buffer
+	    let cmd = DrawCmd {
+            clip_rect: Vec4{x:0., y:0., z:10000., w:10000.},
+            texid: 0,
+            idx_offset: idx_offset as u32,
+            vtx_offset: vtx_offset as u32,
+            elem_count: idx_count,
+        };
+        self.buf_cmd.push(cmd);
+
+        // Reference 
+        let last_cmd = self.buf_cmd.len()-1;
+        (&mut self.buf_cmd[last_cmd],
+         &mut self.buf_idx[idx_offset..idx_offset + idx_count as usize], 
+         &mut self.buf_vtx[vtx_offset..vtx_offset + vtx_count as usize],
+         )
+    }
+}
+
+
 extern "C" {
     fn gb_create_window(
         title: *const c_char,
@@ -120,7 +173,7 @@ extern "C" {
     ) -> &mut Window;
     fn gb_window_destroy(win: *mut Window);
     fn gb_window_start_frame(win: *mut Window, params: &FrameParams) -> &FrameInfo;
-    fn gb_window_render_frame(win: *mut Window, dl: DrawList);
+    fn gb_window_render_frame(win: *mut Window, dl: DrawListC);
     fn gb_set_cursor(win: *mut Window, cursor: Cursor);
     fn gb_create_texture(win: *mut Window, width: c_int, height: c_int, data: *const RGBA)
         -> Texid;
@@ -144,9 +197,18 @@ pub fn window_start_frame(win: *mut Window, params: &FrameParams) -> &FrameInfo 
     unsafe { gb_window_start_frame(win, params) }
 }
 
-pub fn window_render_frame(win: *mut Window, dl: DrawList) {
+pub fn window_render_frame(win: *mut Window, dl: &DrawList) {
+
     unsafe {
-        gb_window_render_frame(win, dl);
+        let dlc = DrawListC {
+            buf_cmd: dl.buf_cmd.as_ptr(),
+            cmd_count: dl.buf_cmd.len() as u32,
+            buf_idx: dl.buf_idx.as_ptr(),
+            idx_count: dl.buf_idx.len() as u32,
+            buf_vtx: dl.buf_vtx.as_ptr(),
+            vtx_count: dl.buf_vtx.len() as u32,
+        };
+        gb_window_render_frame(win, dlc);
     }
 }
 
@@ -168,5 +230,9 @@ pub fn delete_texture(win: *mut Window, texid: Texid) {
     }
 }
 
+pub fn make_color(r: u8, g: u8, b: u8, a: u8) -> RGBA {
+
+    (a as u32)<<24 | (b as u32) <<16 | (g as u32) << 8 | r  as u32
+}
 
 
--- gux/src/main.rs
+++ gux/src/main.rs
@@ -14,6 +14,17 @@ fn main() {
     let win = gb::create_window("Gux".to_string(), 1000, 800, &cfg);
     println!("create_window():{:p}", win);
 
+
+	// // Creates image with one white opaque pixel
+	// var rect [1]RGBA
+	// rect[0] = MakeColor(255, 255, 255, 255)
+	//
+	// // Creates and transfer 1 pixel opaque white texture needed for all commands
+	// texId := win.CreateTexture(1, 1, &rect[0])
+    let pixel = gb::make_color(255,255,255,255);
+    let texid = gb::create_texture(win, 1, 1, &pixel);
+    println!("texid:{texid}");
+
     let fparams = gb::FrameParams {
         ev_timeout: 0.0,
         clear_color: gb::Vec4 {
@@ -24,14 +35,22 @@ fn main() {
         },
     };
 
-    let dl = gb::DrawList {
-        buf_cmd: std::ptr::null_mut(),
-        cmd_count: 0,
-        buf_idx: std::ptr::null_mut(),
-        idx_count: 0,
-        buf_vtx: std::ptr::null_mut(),
-        vtx_count: 0,
-    };
+    let mut draw_list1 = gb::DrawList::new();
+    {
+        let (cmd, idx_buf, vtx_buf) = draw_list1.new_cmd(6, 4);
+        cmd.clip_rect = gb::Vec4{x:0., y:0., z: 4000., w:4000.};
+        cmd.texid = texid;
+        idx_buf[0] = 0;
+        idx_buf[1] = 1;
+        idx_buf[2] = 2;
+        idx_buf[3] = 2;
+        idx_buf[4] = 3;
+        idx_buf[5] = 0;
+        vtx_buf[0] = gb::Vertex{pos: gb::Vec2{x:10., y:10.}, uv: gb::Vec2{x:0., y:0.}, col: 0xFF_FF_00_00};
+        vtx_buf[1] = gb::Vertex{pos: gb::Vec2{x:10., y:100.}, uv: gb::Vec2{x:0., y:0.}, col: 0xFF_FF_00_00};
+        vtx_buf[2] = gb::Vertex{pos: gb::Vec2{x:200., y:100.}, uv: gb::Vec2{x:0., y:0.}, col: 0xFF_FF_00_00};
+        vtx_buf[3] = gb::Vertex{pos: gb::Vec2{x:200., y:10.}, uv: gb::Vec2{x:0., y:0.}, col: 0xFF_FF_00_00};
+    }
 
     loop {
         let frame_info = gb::window_start_frame(win, &fparams);
@@ -42,7 +61,7 @@ fn main() {
             println!("events:{}", frame_info.ev_count);
         }
 
-        gb::window_render_frame(win, dl);
+        gb::window_render_frame(win, &draw_list1);
     }
     gb::window_destroy(win);
 }
